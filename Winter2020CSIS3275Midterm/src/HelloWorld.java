8988c39dd896296d37588a6bda5e162789525ab4
Miguel Ballesteros <ballesterosb@student.douglascollege.ca>
2020-02-14 13:23:14 -0800

    Created a HelloWorld class that prints Name and Email

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Miguel Ballesteros");
+		System.out.println("ballesterosb@student.douglascollege.ca");
+		
+	}
+	
+}
